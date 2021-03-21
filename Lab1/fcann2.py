Lab1: Fixed gradient problems

Lab1/fcann2.py
@@ -12,7 +12,7 @@ def softmax(x):
 
 def cross_entropy_loss(probits, Y_):
     N = probits.shape[0]
-    return np.mean(np.sum(-np.log(probits[range(N), Y_])))
+    return np.mean(np.sum(-np.log(probits[range(N), Y_] + 1e-13)))
 
 
 def fcann2_train(X, Y_, hidden_neurons=5, param_niter=1000, param_delta=0.01, param_lambda=0.05, print_step=100):
@@ -28,8 +28,10 @@ def fcann2_train(X, Y_, hidden_neurons=5, param_niter=1000, param_delta=0.01, pa
     for epoch in range(param_niter):
 
         # forward pass
-        h1 = reLu(np.matmul(X, w_1) + b_1)
-        s2 = np.matmul(h1, w_2) + b_2
+        s1 = np.dot(X, w_1) + b_1
+        h1 = reLu(s1)
+        s2 = np.dot(h1, w_2) + b_2
+        s2 -= s2.max()
         probits = softmax(s2)
         predicted_classes = np.argmax(probits, axis=1)
 
@@ -44,26 +46,26 @@ def fcann2_train(X, Y_, hidden_neurons=5, param_niter=1000, param_delta=0.01, pa
         grads_w_2_tmp[range(N), Y_] -= 1    # predicted classes - y_gt
         grads_w_2_tmp /= N
         grads_b_2 = np.sum(grads_w_2_tmp, axis=0)
-        grads_w_2 = np.matmul(h1.T, grads_w_2_tmp)
+        grads_w_2 = np.dot(h1.T, grads_w_2_tmp)
 
-        grads_w_1_tmp = np.matmul(grads_w_2_tmp, w_2.T)
+        grads_w_1_tmp = np.dot(grads_w_2_tmp, w_2.T)
         grads_w_1_tmp[h1 <= 0.0] = 0.0
-        grads_w_1 = np.mean(np.matmul(X.T, grads_w_1_tmp), axis=0)
+        grads_w_1 = np.dot(X.T, grads_w_1_tmp)
         grads_b_1 = np.sum(grads_w_1_tmp, axis=0)
 
         # updating the weights
 
-        w_1 -= param_delta * grads_w_1 + param_lambda * np.linalg.norm(w_1)
+        w_1 -= param_delta * grads_w_1 + param_lambda * np.linalg.norm(w_1)/N
         b_1 -= param_delta * grads_b_1
 
-        w_2 -= param_delta * grads_w_2 + param_lambda * np.linalg.norm(w_2)
+        w_2 -= param_delta * grads_w_2 + param_lambda * np.linalg.norm(w_2)/N
         b_2 -= param_delta * grads_b_2
 
     return w_1, b_1, w_2, b_2
 
 
 def fcann2_classify(X, w_1, b_1, w_2, b_2):
-    h1 = np.max(0, np.dot(X, w_1) + b_1)
+    h1 = reLu(np.dot(X, w_1) + b_1)
     s2 = np.dot(h1, w_2) + b_2
     probits = softmax(s2)
     predicted_classes = np.argmax(probits, axis=1)

Lab1/mnist_shootout.py
@@ -1,12 +1,12 @@
-import torch
-import torchvision
-import numpy as np
-import pt_deep
-import matplotlib.pyplot as plt
-import data
-
 from torchvision import datasets
 from sklearn import svm
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pt_deep
+import torch
+import data
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 